6ef76a8a979ae36a1cce7ff293da6d07c13753c6
Hackbright Student <info@hackbrightacademy.com>
2014-06-10 17:16:02 -0700

    need to return output as list

--- markov.py
+++ markov.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # a = ['bat','cat', 'rat' , 'gnat']
 import sys
+import random
 
 def make_chains(input_list):
     """Takes an input text as a string and returns a dictionary of
     markov chains."""
     text_chain_dict = {}
     
-    # i = 0
     for i in range(len(input_list)-2):
         key = (input_list[i],input_list[i+1])
         value = input_list[i+2]
@@ -15,14 +15,32 @@ def make_chains(input_list):
             text_chain_dict[key].append(value)
         else:
             text_chain_dict[key] = [value]
-           #text_chain_dict.get(key)
-    # print text_chain_dict   
+           
     return text_chain_dict
 
-# def make_text(chains):
-#     """Takes a dictionary of markov chains and returns random text
-#     based off an original text."""
-#     return "Here's some random text."
+def make_text(chain_dict):
+    """Takes a dictionary of markov chains and returns random text
+    based off an original text."""
+    
+    text_block = []
+
+    start_phrase = random.choice(chain_dict.keys()) 
+    add_phrase = random.choice(chain_dict.get(start_phrase))
+
+    #for index in range(5):
+        
+    next_phrase = (start_phrase[1], add_phrase) 
+        #text_block.append('what')
+        #index += 1
+
+    print start_phrase, add_phrase, next_phrase
+    
+
+        
+    # print phrase
+    # print chain_dict.get(start_phrase)
+
+    # return "Here's some random text."
 
 def main():
     args = sys.argv
@@ -34,18 +52,11 @@ def main():
     input_list = input_text.split()
    
     # print input_list
-  
-# if we were to read line by line we'd do the following
-    # for line in f:
-    #     line = line.rstrip()
-    #     input_text = line.split(' ')
-    #     input_list += input_text
 
-    # print input_list
 
     chain_dict = make_chains(input_list)
-    print chain_dict
-   # random_text = make_text(chain_dict)
+    # print chain_dict
+    random_text = make_text(chain_dict)
     # print random_text
 
 if __name__ == "__main__":
